1c7ce697b85a3dbc89d8ef71b9988557903b1bbf
santosh-daware <santosh.daware@monash.edu>
2025-02-18 18:28:20 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@ window.addEventListener('scroll', scrollActive)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2000,
+    duration: 2,
     delay: 2,
 //     reset: true
 });
